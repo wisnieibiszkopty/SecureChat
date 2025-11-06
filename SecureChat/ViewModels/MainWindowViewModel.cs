6f7c2dc5c222f0d5043b722c5d9040a61b867a29
Kamil Wodowski <kamilwodowski@o2.pl>
2025-11-06 17:17:20 +0100

    Setup Avalonia app

---
+++ SecureChat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SecureChat.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ SecureChat/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SecureChat.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
